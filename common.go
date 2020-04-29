bb07c5632c7412889c32f45b49824e6805eed3ca
Paul Greenberg <greenpau@outlook.com>
2020-04-29 17:30:08 -0400

    add TokenOrigin to CommonTokenConfig

--- common.go
+++ common.go
@@ -6,6 +6,7 @@ type CommonTokenConfig struct {
 	TokenName   string `json:"token_name,omitempty" xml:"token_name" yaml:"token_name"`
 	TokenSecret string `json:"token_secret,omitempty" xml:"token_secret" yaml:"token_secret"`
 	TokenIssuer string `json:"token_issuer,omitempty" xml:"token_issuer" yaml:"token_issuer"`
+	TokenOrigin string `json:"token_origin,omitempty" xml:"token_origin" yaml:"token_issuer"`
 	// The expiration time of a token in seconds
 	TokenLifetime      int    `json:"token_lifetime,omitempty" xml:"token_lifetime" yaml:"token_lifetime"`
 	TokenSigningMethod string `json:"token_signing_method,omitempty" xml:"token_signing_method" yaml:"token_signing_method"`
